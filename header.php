4de3e6073a77615fd320eeff07069747275dfa5d
Tiafeno <tiafenofnel@gmail.com>
2019-06-25 15:57:17 +0300

    add google ads

---
+++ header.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Yozi
+ * @since Yozi 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php
+	if ( !function_exists( 'wp_site_icon' ) ) {
+		$favicon = yozi_get_config('media-favicon');
+		if ( (isset($favicon['url'])) && (trim($favicon['url']) != "" ) ) {
+	        if (is_ssl()) {
+	            $favicon_image_img = str_replace("http://", "https://", $favicon['url']);		
+	        } else {
+	            $favicon_image_img = $favicon['url'];
+	        }
+		?>
+	    	<link rel="shortcut icon" href="<?php echo esc_url($favicon_image_img); ?>" />
+	    <?php } ?>
+    <?php } ?>
+
+	<?php wp_head(); ?>
+    
+    <!-- Global site tag (gtag.js) - Google Ads: 862756431 --> 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-862756431"></script>
+    <script> 
+        window.dataLayer = window.dataLayer || []; 
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date()); 
+        gtag('config', 'AW-862756431'); 
+    </script> 
+    
+    <!-- Event snippet for Inscription sur le site conversion page --> 
+    <script> gtag('event', 'conversion', {'send_to': 'AW-862756431/1PH2CJqjm6MBEM-8spsD'}); </script> 
+    
+</head>
+<body <?php body_class(); ?>>
+<?php if ( yozi_get_config('preload', true) ) {
+	$preload_icon = yozi_get_config('media-preload-icon');
+	$preload_icon_image_img = '';
+	if ( (isset($preload_icon['url'])) && (trim($preload_icon['url']) != "" ) ) {
+        if (is_ssl()) {
+            $preload_icon_image_img = str_replace("http://", "https://", $preload_icon['url']);		
+        } else {
+            $preload_icon_image_img = $preload_icon['url'];
+        }
+    }
+?>
+	<div class="apus-page-loading">
+        <div class="apus-loader-inner" style="<?php echo esc_attr($preload_icon_image_img ? 'background-image: url(\''.$preload_icon_image_img.'\')' : ''); ?>"></div>
+    </div>
+<?php } ?>
+<div id="wrapper-container" class="wrapper-container">
+
+	<?php get_template_part( 'headers/mobile/offcanvas-menu' ); ?>
+	<?php get_template_part( 'headers/mobile/header-mobile' ); ?>
+	
+	<?php $header = apply_filters( 'yozi_get_header_layout', yozi_get_config('header_type') );
+		if ( empty($header) ) {
+			$header = 'v1';
+		}
+	?>
+	<?php get_template_part( 'headers/'.$header ); ?>
+	<div id="apus-main-content">
