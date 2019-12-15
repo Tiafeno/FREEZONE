a803c65c7cbcc8f0b4258b7313fef0f5f0af1962
Tiafeno <tiafenofnel@gmail.com>
2019-12-15 17:08:47 +0300

    up version for fix

--- assets/js/fz-custom.js
+++ assets/js/fz-custom.js
@@ -1,7 +1,11 @@
 (function ($) {
     "use strict";
-    var $item = $("ul.product-categories").find('.current-cat-parent');
-    if ($item.length > 0) {
-        $item.find('i').trigger("click");
-    }
+
+    $(document).ready(function () {
+        var $item = $("ul.product-categories").find('.current-cat-parent');
+        if ($item.length > 0) {
+            $item.find('i').trigger("click");
+        }
+    });
+
 })(jQuery);
